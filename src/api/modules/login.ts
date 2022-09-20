72b29b0d102e67633da29fb82bd2b11188bc8d7a
VANCEE LEE <1280403599@qq.com>
2022-09-20 23:01:44 +0800

    表格布局

--- src/api/modules/login.ts
+++ src/api/modules/login.ts
@@ -1,11 +1,12 @@
 import { Login } from '@/api/interface/index'
 import { PORT1 } from '@/api/config/servicePort'
 import http from '@/api'
+import { LoginFrom } from '@/views/login/types'
 
 /**
  * @name 登录模块
  */
 // * 用户登录接口
-export const loginApi = (params: Login.LoginParams) => {
+export const loginApi = (params: LoginFrom) => {
   return http.post<Login.LoginAPI>(PORT1 + `/web/v1.0/user/login-by-name-passwd`, params)
 }
